6b03585b6c4a9e2e30ca0631e036205b8bd4571c
unknown <rfehra@gmail.com>
2023-02-11 18:34:01 +0100

    last initial

--- app/src/main/java/com/example/learning/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/learning/ui/settings/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -23,20 +24,44 @@ import com.example.learning.ui.viewBinding
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
     private val binding by viewBinding(FragmentSettingsBinding::bind)
-
     private val CHANNEL_ID = "channel_id"
     private val notificationID = 101
+    private val sharedPreferences by lazy {
+        activity?.getSharedPreferences("prefs", Context.MODE_PRIVATE)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val settingsViewModel =
             ViewModelProvider(this).get(SettingsViewModel::class.java)
 
-        createNotificationChannel()
-        binding.button.setOnClickListener {
-            sendNotification()
+        binding.button.isEnabled = false
+        updateButtonState()
+        binding.btnSwitch.isChecked = sharedPreferences?.getBoolean("switch_state", false) ?: false
+        binding.btnSwitch.setOnCheckedChangeListener { _, isChecked ->
+            sharedPreferences?.edit()?.putBoolean("switch_state", isChecked)?.apply()
+            binding.button.isEnabled = isChecked
+            createNotificationChannel()
+            binding.button.setOnClickListener {
+                sendNotification()
+            }
+            updateButtonState()
         }
     }
+    override fun onResume() {
+        super.onResume()
+        updateButtonState()
+        if (binding.btnSwitch.isChecked) {
+            binding.button.setOnClickListener {
+                sendNotification()
+            }
+        }
+    }
+
+    private fun updateButtonState() {
+        binding.button.isEnabled = binding.btnSwitch.isChecked
+    }
+
     private fun createNotificationChannel(){
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
