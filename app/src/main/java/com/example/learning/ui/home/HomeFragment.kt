43da0152be439b0dcce7579b3405a60f0abdad74
unknown <rfehra@gmail.com>
2023-02-14 13:46:59 +0100

    touch screen counter added

--- app/src/main/java/com/example/learning/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/learning/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.example.learning.ui.home
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,36 +9,37 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.learning.R
 import com.example.learning.databinding.FragmentHomeBinding
+import com.example.learning.ui.settings.SettingsViewModel
+import com.example.learning.ui.viewBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+    private lateinit var touchCounter: TextView
+    private var count = 0
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        count = sharedPreferences.getInt("count", 0)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val settingsViewModel =
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
+
+        touchCounter = view.findViewById(R.id.touch_counter)
+        touchCounter.text = count.toString()
+
+        binding.root.setOnTouchListener  { _, _ ->
+            count++
+            touchCounter.text = count.toString()
+            val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+            sharedPreferences.edit().putInt("count", count).apply()
+            true
+        }
     }
 }
